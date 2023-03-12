20a60dccc58fd6909785cd02053be44cc02f623f
VishalKhandelwal <vishalshekhar9316@gmail.com>
2023-03-12 21:09:35 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all ml related code here")
+
+
+def model1():
+    print("adding code for Ml Model1")
+
+def model2():
+    print("adding code for Ml Model2")
+
+def model3():
+    print("adding code for Ml Model3")
